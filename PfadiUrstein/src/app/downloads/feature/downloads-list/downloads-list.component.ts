c1082dfde10db6c952adac3c6e3ea4376a94eec6
AnikaAe13 <anika.aepli@edu.gbssg.ch>
2025-02-03 19:06:10 +0100

    seiten erstellt

---
+++ PfadiUrstein/src/app/downloads/feature/downloads-list/downloads-list.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-downloads-list',
+  imports: [],
+  templateUrl: './downloads-list.component.html',
+  styleUrl: './downloads-list.component.css'
+})
+export class DownloadsListComponent {
+
+}
---
+++ PfadiUrstein/src/app/schnuppern/feature/schnuppern-list/schnuppern-list.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-schnuppern-list',
+  imports: [],
+  templateUrl: './schnuppern-list.component.html',
+  styleUrl: './schnuppern-list.component.css'
+})
+export class SchnuppernListComponent {
+
+}

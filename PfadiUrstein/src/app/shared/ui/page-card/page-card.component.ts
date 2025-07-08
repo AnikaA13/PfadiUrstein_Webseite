3de23b3cfa42b3c8512f9283ace624af55241886
Anika <148954907+AnikaA13@users.noreply.github.com>
2025-07-08 12:09:58 +0000

    create page card

---
+++ PfadiUrstein/src/app/shared/ui/page-card/page-card.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pagecard',
+  imports: [],
+  templateUrl: './page-card.component.html',
+  styleUrl: './page-card.component.css',
+})
+
+export class PageCardComponent {
+
+  
+}

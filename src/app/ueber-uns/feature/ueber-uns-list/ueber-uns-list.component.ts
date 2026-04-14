9e85757b9a23ee07cb42d5576e2d15fd47b429cb
AnikaAe13 <anika.aepli@edu.gbssg.ch>
2026-04-14 18:31:18 +0200

    opdet

--- src/app/ueber-uns/feature/ueber-uns-list/ueber-uns-list.component.ts
+++ src/app/ueber-uns/feature/ueber-uns-list/ueber-uns-list.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from '../../../shared/ui/header/header.component';
+import { PageCardComponent } from '../../../shared/ui/page-card/page-card.component';
 
 @Component({
   selector: 'app-ueber-uns-list',
-  imports: [],
+  imports: [HeaderComponent, PageCardComponent],
   templateUrl: './ueber-uns-list.component.html',
-  styleUrl: './ueber-uns-list.component.css'
+  styleUrl: './ueber-uns-list.component.css',
 })
-export class UeberUnsListComponent {
-
-}
+export class UeberUnsListComponent {}

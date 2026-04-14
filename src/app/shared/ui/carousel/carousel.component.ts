47b5e9b4e1bdae4445d4da6cb39227052f04f9cd
AnikaAe13 <anika.aepli@edu.gbssg.ch>
2026-04-14 19:06:18 +0200

    update to angular 21

--- src/app/shared/ui/carousel/carousel.component.ts
+++ src/app/shared/ui/carousel/carousel.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+
 
 @Component({
   selector: 'app-carousel',
   standalone: true,
-  imports: [CommonModule],
+  imports: [],
   templateUrl: './carousel.component.html',
 })
 export class CarouselComponent {
